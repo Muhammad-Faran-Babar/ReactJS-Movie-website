b29e04c00e13e57aa2a27ac06e6323b2b0ff2c71
faran babar <faribabar1998@gmail.com>
2022-12-16 10:37:18 +0500

    initial commit

---
+++ src/Movie.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useGlobalContext } from "./context";
+
+const imgUrl = "https://via.placeholder.com/200/200";
+
+const Movie = () => {
+  const { movie, isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <div className="loading">Loading....</div>;
+  }
+
+
+       
+
+  return (
+    <>
+      {/* if movie is present then only show data else remain as it is  */}
+      <section className="movie-page">
+        <div className="grid grid-4-col">
+          {movie
+            ? movie.map((curMovieElem) => {
+                const { imdbID, Title, Poster } = curMovieElem;
+                const movieName = Title.substring(0, 15);
+
+                return (
+                  
+                    <div className="card">
+                      <div className="card-info">
+                        
+                        <img src={Poster === "N/A" ? imgUrl : Poster} alt="#" />
+                        <h2>
+                          {movieName.length > 13
+                            ? `${movieName}...`
+                            : movieName}
+                        </h2>
+                        <NavLink to={`movie/${imdbID}`} key={imdbID} className="back-btn"> movie details</NavLink>
+
+                      </div>
+
+                    </div>
+                  
+                );
+              })
+            : ""}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Movie;
